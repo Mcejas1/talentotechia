1342ef2e00ae103ebadad0c8791f63dcc828c832
Mcejas1 <marcoscejas41@gmail.com>
2024-11-21 22:36:49 -0300

    Add files via upload

--- clase6.py
+++ clase6.py
@@ -1,1 +1,24 @@
+'''
+LIBERERIA
+3 acciones 
+instalacion
+    se haceen la terminal
+importacion
+    se aplica en un archivo
+    import pandas as pd---------- abreviacion= apodo
+    from pandas import max------------ se importa solo lo que se va usar en la ocasion.
+    from panda import *-------=se importa todo por separado
+uso
 
+
+'''
+
+import streamlit as st
+
+'''
+MODELOS = [
+    'llama3-8b-8192',
+    'llama3-70b-8192',
+    'mixtral-8x7b-32768'
+
+    '''
